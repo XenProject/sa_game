d8282bd0384c5ee8203e895de5df8a8157be1b6c
Xen <klimovjeka0987@gmail.com>
2017-09-22 14:17:40 +0300

    no message

---
+++ scripts/vscripts/heroes/vasya/vasya_clear_sky.lua
@@ -0,0 +1,33 @@
+vasya_clear_sky = class({})
+
+function vasya_clear_sky:GetAOERadius()
+	return self:GetSpecialValueFor("max_radius")/2.0
+end
+
+function vasya_clear_sky:OnSpellStart()
+	local caster = self:GetCaster()
+	local point = self:GetCursorPosition()
+	local radius = RandomInt(0, self:GetSpecialValueFor("max_radius"))
+	local damage = self:GetSpecialValueFor("damage")
+
+	local nFXIndex = ParticleManager:CreateParticle( "particles/units/heroes/hero_lina/lina_spell_light_strike_array.vpcf", PATTACH_WORLDORIGIN, nil )
+	ParticleManager:SetParticleControl( nFXIndex, 0, point )
+	ParticleManager:SetParticleControl( nFXIndex, 1, Vector( radius, 1, 1 ) )
+	ParticleManager:ReleaseParticleIndex( nFXIndex )
+
+	local enemyUnits = FindUnitsInRadius(caster:GetTeamNumber(), point,
+				nil, radius,
+	    		DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_ALL,
+	        	DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
+
+	for _,unit in pairs(enemyUnits) do
+		local damageTable = {
+			victim = unit,
+			attacker = caster,
+			damage = damage,
+			damage_type = DAMAGE_TYPE_MAGICAL,
+			ability = self
+		}
+		ApplyDamage(damageTable)
+	end
+end
